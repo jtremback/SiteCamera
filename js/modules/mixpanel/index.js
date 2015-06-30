01b80d137f855194cef51f79e57950f42978f3c9
Jehan <jehan.tremback@gmail.com>
2015-06-30 00:20:33 -0700

    prevent spooky getter cache mutation

--- js/modules/mixpanel/index.js
+++ js/modules/mixpanel/index.js
@@ -28,7 +28,7 @@ exports.events = function events (config, event, properties) {
 
   let message = {
     event: event,
-    properties: Object.assign(config, properties)
+    properties: Object.assign({}, config, properties)
   }
 
   return transmit('https://api.mixpanel.com/track/', message)
@@ -36,7 +36,7 @@ exports.events = function events (config, event, properties) {
 
 exports.transmit = transmit
 function transmit (baseUrl, message) {
-  // console.log(baseUrl, message)
+  console.log('mixpanel', baseUrl, message)
   const promise = fetch(`${baseUrl}?data=${base64.encode(JSON.stringify(message))}&verbose=1`).then((res) => {
     return res.text()
   })
