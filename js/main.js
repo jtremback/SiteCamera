e69f76e5d5af0339289dbb4581c9dc84660c9c98
Jehan <jehan.tremback@gmail.com>
2015-07-01 17:08:57 -0700

    start work on sign in screen

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 const React = require('react-native')
 const actions = require('./actions.js')
 const MainNavigator = require('./components/MainNavigator.js')
-const { StatusBarIOS } = React
+const {
+  StatusBarIOS,
+  Text
+} = React
+const flux = require('./flux.js')
+const getters = require('./getters.js')
 
 module.exports = React.createClass({
   displayName: 'Frame',
-  componentDidMount () {
-    actions.initApp().catch(console.error)
-    StatusBarIOS.setStyle('light-content', true)
+  mixins: [flux.ReactMixin],
+
+  getDataBindings () {
+    return {
+      dropboxAccessToken: getters.dropboxAccessToken
+    }
   },
+
+  componentDidMount () {
+    // actions.initApp().catch(console.error)
+    // StatusBarIOS.setStyle('light-content', true)
+  },
+
   render () {
     return (
-      <MainNavigator />
+      this.state.dropboxAccessToken ?
+        <MainNavigator />
+      :
+        <Text>log in with dropbox FFS</Text>
     )
   }
 })
