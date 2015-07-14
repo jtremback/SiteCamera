b6641d02f56a706339ab8b771642370f93c6fe6b
Jehan <jehan.tremback@gmail.com>
2015-07-13 17:47:02 -0700

    more updates

--- js/components/CameraScreen.js
+++ js/components/CameraScreen.js
@@ -1,14 +1,16 @@
-var React = require('react-native')
-var {
+const React = require('react-native')
+const {
   StyleSheet,
   Text,
   View,
   TouchableHighlight,
   Image,
   PropTypes,
+  NativeModules
 } = React
 
-var Camera = require('react-native-camera')
+const Camera = require('react-native-camera')
+const { Accelerometer } = NativeModules
 
 var colors = require('../styles/colors.js')
 const styles = StyleSheet.create({
@@ -92,27 +94,54 @@ module.exports = React.createClass({
     }
   },
 
+  componentDidMount () {
+    Accelerometer.startAccelerometerUpdates()
+  },
+
+  componentWillUnmount () {
+    Accelerometer.stopAccelerometerUpdates()
+  },
+
   takePicture () {
-    const capture = location => {
+    const capture = (location, rotation) => {
       this.refs.cam.capture({
-        metadata: { location }
+        metadata: { location },
+        rotation: rotation
       }, (err, path) => {
+        console.log(err)
         if (!err) {
           this.props.tookPhoto(path)
         }
       })
     }
 
-    navigator.geolocation.getCurrentPosition(
-      location => {
-        console.log(location)
-        capture(location)
-      },
-      () => {
-        capture(null)
-      },
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    )
+    Accelerometer.getAccelerometerData((err, data) => {
+      const angle = Math.atan2(data.y, -data.x)
+      let rotation
+      if (angle >= -2.25 && angle <= -0.75) {
+        rotation = 270
+      }
+      else if (angle >= -0.75 && angle <= 0.75) {
+        rotation = 0
+      }
+      else if (angle >= 0.75 && angle <= 2.25) {
+        rotation = 90
+      }
+      else if (angle <= -2.25 || angle >= 2.25) {
+        rotation = 180
+      }
+
+      navigator.geolocation.getCurrentPosition(
+        location => {
+          capture(location, rotation)
+        },
+        () => {
+          capture(null)
+        },
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+      )
+    })
+
   },
 
   render () {
